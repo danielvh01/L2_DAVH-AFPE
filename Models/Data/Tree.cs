47feade46d962d4e8203a4937b739f96dbffea85
danielvh01 <danielvelasquez32@gmail.com>
2021-02-23 18:01:31 -0600

    Node 1.0

---
+++ Models/Data/Tree.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace L2_DAVH_AFPE.Models.Data
+{
+    public class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+
+    }
+}
